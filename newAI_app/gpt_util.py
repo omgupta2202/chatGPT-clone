2dbf073f9622ef7a741f10b45b0f68e758bd8260
Om Gupta <66220328+omgupta2202@users.noreply.github.com>
2024-05-19 02:15:00 +0530

    Update gpt_util.py

--- newAI_app/gpt_util.py
+++ newAI_app/gpt_util.py
@@ -2,31 +2,18 @@ import os
 import openai
 import requests
 
-API_ENDPOINT = "https://icd-finetuned-api.vercel.app/AI/chat"
 
-def get_response(prompt):
-    data = {"q": prompt}
-    headers = {"Content-Type": "application/json"}
-    print (data)
-    response = requests.post(API_ENDPOINT, json=data, headers=headers)
-    print(response.text)
-    if response.status_code == 200:
-        return response.text
-    else:
-        return "Error in API request."
+# API_KEY = os.environ['OPENAI_API_KEY']
+API_KEY = 'Your_api_key'
+openai.api_key = API_KEY
 
 
-# # API_KEY = os.environ['OPENAI_API_KEY']
-# API_KEY = '943OVjNXFK7RJMVPklo7T3BlbkFJnx1YersHJf8G86vLM5s-H'
-# openai.api_key = API_KEY
-
-
-# def get_response(prompt, model="gpt-3.5-turbo"):
-#     messages = [
-#         {"role": "user", "content": prompt}]
-#     response = openai.ChatCompletion.create(
-#         model=model,
-#         messages=messages,
-#         temperature=0.8,
-#     )
-#     return response.choices[0].message['content']
+def get_response(prompt, model="gpt-3.5-turbo"):
+    messages = [
+        {"role": "user", "content": prompt}]
+    response = openai.ChatCompletion.create(
+        model=model,
+        messages=messages,
+        temperature=0.8,
+    )
+    return response.choices[0].message['content']
